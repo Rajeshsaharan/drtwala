722fd56ea193fbe3c8b4bc6701a7ad86035f16cb
rajeshsaharan <rajeshsharan1607@gmail.com>
2024-01-10 18:16:34 +0530

    initial

---
+++ android/app/src/main/kotlin/com/example/drtwala/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.drtwala
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/Theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//created by :- rajesh j saharan on jan 9 2023
+
+class Theme {
+  static Color primaryColor = const Color(0xFFfbf5f3);
+  static Color secondaryColor = const Color(0xff000022);
+  static Color trianryColor = const Color(0xffe28413);
+}
