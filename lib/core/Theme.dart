fc5593950845959bba0a467c3a0d6bb422364290
rajeshsaharan <rajeshsharan1607@gmail.com>
2024-01-11 21:03:49 +0530

    added global things

--- lib/core/Theme.dart
+++ lib/core/Theme.dart
@@ -6,4 +6,7 @@ class Theme {
   static Color primaryColor = const Color(0xFFfbf5f3);
   static Color secondaryColor = const Color(0xff000022);
   static Color trianryColor = const Color(0xffe28413);
+
+
+  Static 
 }
